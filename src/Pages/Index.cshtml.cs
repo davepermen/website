2daa74009f8d0436a407a28e9580637f15bdbaad
David Spörri <davepermen@hotmail.com>
2021-04-09 17:37:13 +0200

    cleaned up webpage

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Davepermen.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
