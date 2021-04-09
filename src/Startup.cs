a74c187c9056ba9667ad1e3bf8236962663ea22b
David Spörri <davepermen@hotmail.com>
2021-04-09 18:21:01 +0200

    using hosting defaults

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,3 +1,4 @@
+using Conesoft.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,41 +15,7 @@ namespace Davepermen.Website
         {
             app.UseRouting();
 
-            app.Use(async (context, next) =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html; charset=utf-8");
-                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                await next.Invoke();
-            });
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = context =>
-                {
-                    if (context.Context.Response.Headers["Content-Type"].Count > 0)
-                    {
-                        var contentType = context.Context.Response.Headers["Content-Type"][0];
-                        if (contentType.Contains("text") || contentType.Contains("json") || contentType.Contains("xml"))
-                        {
-                            context.Context.Response.Headers["Content-Type"] = contentType + "; charset=utf-8";
-                        }
-                    }
-                    if(context.Context.Response.Headers["Cache-Control"].Count == 0)
-                    {
-                        context.Context.Response.Headers.Add("Cache-Control", "max-age=31536000, immutable");
-                    }
-                }
-            });
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Response.Headers["Cache-Control"].Count == 0)
-                {
-                    context.Response.Headers.Add("Cache-Control", "no-cache");
-                }
-                await next.Invoke();
-            });
+            app.UseHostingDefaults(useDefaultFiles: true, useStaticFiles: true);
 
             app.UseEndpoints(endpoints =>
             {
