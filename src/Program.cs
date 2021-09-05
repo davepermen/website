e73b0da8ba87f2db744a30dd6aa48f2c794bc56f
David Spörri <davepermen@hotmail.com>
2021-09-05 16:50:17 +0200

    cleaned up thanks to @davidfowl 👍
    playing with net6 is a lot of fun so far

--- src/Program.cs
+++ src/Program.cs
@@ -5,11 +5,7 @@ builder.Configuration.AddHostConfiguration();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-app.UseRouting();
 app.UseHostingDefaults(useDefaultFiles: true, useStaticFiles: true);
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapRazorPages();
-});
+app.MapRazorPages();
 
 app.Run();
