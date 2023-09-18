6743fe4791bb87cea4c778a9247b02bac785540b
David Spörri <davepermen@hotmail.com>
2023-09-18 10:00:04 +0200

    .net 8

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,13 @@ builder.Configuration.AddHostConfiguration();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+Console.WriteLine(app.Environment.EnvironmentName);
 app.UseHostingDefaults(useDefaultFiles: true, useStaticFiles: true);
+app.UseStaticFiles(new StaticFileOptions
+{
+    ServeUnknownFileTypes = true,
+    DefaultContentType = "application/jrd+json; charset=utf-8",
+});
 app.MapRazorPages();
 
 app.Run();
