d9135768cc62cf9d1797f09de286a41f4e2e764f
David Spörri <davepermen@hotmail.com>
2025-01-01 22:09:57 +0100

    small updates

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,27 @@
 using Conesoft.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddHostConfiguration();
-builder.Services.AddRazorPages();
+
+builder
+    .AddHostConfigurationFiles(legacyMode: false)
+    .AddUsersWithStorage()
+    .AddHostEnvironmentInfo()
+    .AddLoggingService()
+    ;
+
+builder.Services
+    .AddCompiledHashCacheBuster()
+    .AddRazorPages();
 
 var app = builder.Build();
-Console.WriteLine(app.Environment.EnvironmentName);
-app.UseHostingDefaults(useDefaultFiles: true, useStaticFiles: true);
-app.UseStaticFiles(new StaticFileOptions
-{
-    ServeUnknownFileTypes = true,
-    DefaultContentType = "application/jrd+json; charset=utf-8",
-});
+app
+    .UseCompiledHashCacheBuster()
+    .UseStaticFiles(new StaticFileOptions
+    {
+        ServeUnknownFileTypes = true,
+        DefaultContentType = "application/jrd+json; charset=utf-8",
+    });
+
 app.MapRazorPages();
 
 app.Run();
