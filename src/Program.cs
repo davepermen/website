4ba045e9b565bdfd57f8acf7b60e48c2478b4e9f
David Spörri <davepermen@hotmail.com>
2025-01-12 21:23:34 +0100

    cleanup

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using Conesoft.Hosting;
+using Conesoft.PwaGenerator;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,7 @@ app
         DefaultContentType = "application/jrd+json; charset=utf-8",
     });
 
+app.MapPwaInformationFromAppSettings();
 app.MapRazorPages();
 
 app.Run();
